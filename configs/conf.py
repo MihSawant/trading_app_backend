c8ade8058e6b388b3bd9c03b7c7ef713094124a2
Mihir Sawant <mihirssawant06@gmail.com>
2023-02-14 15:46:53 +0530

    added app configs db req data

---
+++ configs/conf.py
@@ -0,0 +1,6 @@
+import bcrypt as bcypt
+class Config :
+    db_collections = []
+    db_username = "unigo_db"
+    db_pwd = "oB7YYc5P7ozwzqAJzUWLBhI3EsQUfS8daHteGmw8"
+    salt = b'$2b$12$7cacwHWHRxf2PTeOVNoSje'
