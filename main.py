4b6f85000143a8c7d292a6aa7833674e7434c739
Mihir Sawant <mihirssawant06@gmail.com>
2023-02-15 21:14:03 +0530

    get favourite stocks
    endpoint added for getting users fav stocks selected

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
 import db.db_conn as db_setup
-from services import stock_search, check_pno, user_login
+from services import stock_search, check_pno, user_login, angel_login
 import bson.json_util as json_util
 from fastapi import websockets, WebSocket ,WebSocketDisconnect
 from models import users, stock_users
 import uvicorn
 
-app = FastAPI()
+app = FastAPI(
+    title="Trade App Backend (Elan and nVision)",
+    version="1.0"
+)
 print("application startup was successfull...")
 # first connect to db
 # db_setup.connect_db()
-@app.get("/")
+@app.get("/", tags=["Hello World!"])
 def hello():
     return {"message" : "Welcome !"}
 
@@ -33,7 +36,7 @@ async def stock_search_by_name_ws(webs: WebSocket):
 
 
 @app.post("/new-user/register")
-def new_user(user_details: users.User):
+def new_user(user_details: users.User,):
     response = users.insert_new_user(user_details)
     return response
 
@@ -51,5 +54,8 @@ def check_if_user_exists(phone_no: users.User_Check_Pno):
 def login(data : users.User_Login):
     return user_login.verify_pin(data)
 
+@app.post("/user/favourite-stocks")
+async def get_favourite_stocks_by_user_id(data: stock_users.Favourite_Stock):
+    return await stock_users.find_stocks_details_by_user_id(data.user_id)
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8090, log_level="info")
