b7a3268976438b2de1fee1b4e154542cd13ceeeb
Mihir Sawant <mihirssawant06@gmail.com>
2023-02-15 21:13:17 +0530

    stock search
    get stock info by id added

--- services/stock_search.py
+++ services/stock_search.py
@@ -1,8 +1,14 @@
 import db.db_conn as db_conn
 
 db = db_conn.connect_db()
+stocks = db.get_collection("stock_list")
 
 def find_stock_by_name(name):
-    stocks = db.get_collection("stock_list")
     return stocks.find({"name" : {"$regex" : name}}, {"_id" : 0})
 
+def find_all_stocks():
+    return stocks.find({})
+
+async def find_by_id(stock_id):
+    return stocks.find_one({"_id" : stock_id}, 
+    {"_id":0, "expiry":0, "lotsize":0, "instrumenttype":0, "tick_size" :0, "strike":0})
