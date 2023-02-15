06a358493070afbe551131e6cad5480166a2bc3d
Mihir Sawant <mihirssawant06@gmail.com>
2023-02-15 21:10:08 +0530

    get stocks of users
    modified update function and get fav stocks added

--- models/stock_users.py
+++ models/stock_users.py
@@ -1,13 +1,20 @@
 from pydantic import BaseModel
 import db.db_conn as db_conn
-import json
+from bson import json_util
 import typing
+from bson.objectid import ObjectId
+import itertools
+from services import stock_search
+
 db = db_conn.connect_db()
-stock_users = db.get_collection("stock_users")
+stock_users = db.get_collection("stocks_users")
 
 class Stock_User(BaseModel):
     stock_ids : typing.List
     user_id : str
+
+class Favourite_Stock(BaseModel):
+    user_id: str
 
 
 def insert_favourite_stock(stock_user_data : Stock_User):
@@ -22,9 +29,9 @@ def insert_favourite_stock(stock_user_data : Stock_User):
     else:
        fav_stocks = find_favourite_stocks_by_user_id(user_id)
        print(fav_stocks)
-       return stock_users.update_one({
-        "stock_ids" : stock_user_data.stock_ids,
-        }, {"user_id" : stock_user_data.user_id})
+       return stock_users.update_one(
+        {"user_id" : stock_user_data.user_id},
+        { "$set" : {"stock_ids" : stock_user_data.stock_ids}})
     
    
 
@@ -34,3 +41,13 @@ def find_favourite_stocks_by_user_id(user_id):
 
 def find_favourite_stocks_by_user_id(user_id):
     return stock_users.find_one({"user_id" : user_id}, {"-user_id":0})
+
+async def find_stocks_details_by_user_id(user_id):
+    fav_stocks_list = stock_users.find_one({"user_id" : user_id}, {"_id":0, "user_id":0})
+    stock_details = []
+
+    stocks = list(itertools.chain.from_iterable(fav_stocks_list.values()))
+    for stock in stocks:
+      stock_details.append(await stock_search.find_by_id(ObjectId(stock)))
+    
+    return json_util._json_convert(stock_details)
