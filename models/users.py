a9de5a172d054379e4c881d720c96c20d08fcd56
Mihir Sawant <mihirssawant06@gmail.com>
2023-02-15 09:06:45 +0530

    change email to phone no.

--- models/users.py
+++ models/users.py
@@ -1,6 +1,7 @@
 import db.db_conn as db_conn
 import configs.conf as c
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, ValidationError
+import bcrypt
 
 db = db_conn.connect_db()
 
@@ -9,17 +10,22 @@ users = db.get_collection("users")
 class User(BaseModel):
     first_name : str
     last_name: str
-    email_id: str
+    phone_no: str
     pin: str
 
+    @validator("phone_no")
+    def phone_no_check(cls, pno):
+        if len(pno) != 10:
+            raise ValueError("phone no. must be of 10 digits")
+        return pno
 
 def insert_new_user(user_details: User): 
-   
+    enc_pin = bcrypt.hashpw(user_details.pin.encode(), c.Config.salt)
     user_data_enc = {
         "first_name" : user_details.first_name,
         "last_name" : user_details.last_name,
-        "email_id" : user_details.email_id,
-        "pin" : user_details.pin
+        "phone_no" : user_details.phone_no,
+        "pin" : enc_pin
     }
     val = users.insert_one(user_data_enc)
     return val.inserted_id
