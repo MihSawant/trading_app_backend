4981d25ce7ea8e931f6d87f90c6a683c66ca4b5e
Mihir Sawant <mihirssawant06@gmail.com>
2023-02-16 16:21:22 +0530

    documentation updated for all apis

--- main.py
+++ main.py
@@ -37,30 +37,30 @@ async def stock_search_by_name_ws(webs: WebSocket):
         print("Connection close")
 
 
-@app.post("/new-user/register")
+@app.post("/new-user/register", tags=["Register New User"])
 def new_user(user_details: users.User,):
     response = users.insert_new_user(user_details)
     return response
 
-@app.post("/user/add-watchlist")
+@app.post("/user/add-watchlist", tags=["Add Favourite stocks for user"])
 def stock_user_insert(stock_data: stock_users.Stock_User):
     stock_users.insert_favourite_stock(stock_data)
 
-@app.post("/user/index")
+@app.post("/user/index", tags=["Check If User Already Exists (contact page -- uis)"])
 def check_if_user_exists(phone_no: users.User_Check_Pno):
     return json_util._json_convert({
         "code" : check_pno.find_user_by_pno(phone_no)
     })
 
-@app.post("/user/login")
+@app.post("/user/login", tags=["User Login"])
 def login(data : users.User_Login):
     return user_login.verify_pin(data)
 
-@app.post("/user/favourite-stocks")
+@app.post("/user/favourite-stocks", tags=["Fetch All Favourite Stocks of User (by uid)"])
 async def get_favourite_stocks_by_user_id(data: stock_users.Favourite_Stock):
     return await stock_users.find_stocks_details_by_user_id(data.uid)
 
-@app.post("/user/check-login-access")
+@app.post("/user/check-login-access", tags=["Angel Auth Details for User (by uid)"])
 def check_user_access(data: users.User_Access):
     return json_util._json_convert(user_access.check_access(data.uid))
 # print(stock_history.get_history())
