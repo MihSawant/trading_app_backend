9b1d2c61ea305d213ee2ce01d41edf0b6e9fe32a
Mihir Sawant <mihirssawant06@gmail.com>
2023-02-16 16:12:18 +0530

    user access
    updated user access to find by uid instead of uid

--- services/user_access.py
+++ services/user_access.py
@@ -3,9 +3,9 @@ import db.db_conn as db_conn
 
 db = db_conn.connect_db()
 
-def check_access(user_id):
+def check_access(uid):
     auth_details = db.get_collection("angel_auth")
-    u = user.find_by_id(user_id)
+    u = user.find_by_uid(uid)
     if u is not None:
         return auth_details.find_one({"name" : "angel"}, {"name" : 0, "_id" : 0}) 
     else:
