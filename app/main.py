1ed2599cf5f84e7bf8f728fd5b7174f4fa4da776
Mihir Sawant <mihirssawant06@gmail.com>
2023-02-17 22:54:58 +0530

    importing angel_login for auth and ref tokens

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request, Response
 import db.db_conn as db_setup
 from services import stock_search, check_pno, user_login, user_access
 # only to on when ref token is needed...
-# from services import angel_login
+from services import angel_login
 import bson.json_util as json_util
 from fastapi import websockets, WebSocket ,WebSocketDisconnect
 from models import users, stock_users, users_orders
