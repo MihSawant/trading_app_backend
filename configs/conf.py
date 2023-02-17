e2fb39e82037a7731fe762f0a9e1e5e1563f3d27
Mihir Sawant <mihirssawant06@gmail.com>
2023-02-17 22:44:41 +0530

    removed the files from directory parent

--- configs/conf.py
+++
@@ -1,6 +0,0 @@
-import bcrypt as bcypt
-class Config :
-    db_collections = []
-    db_username = "unigo_db"
-    db_pwd = "oB7YYc5P7ozwzqAJzUWLBhI3EsQUfS8daHteGmw8"
-    salt = b'$2b$12$7cacwHWHRxf2PTeOVNoSje'
